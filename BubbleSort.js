36c100152ff65d14f98e1458df9a96495e329fbf
Vasyl-Mokhnatchuk <144223353+Vasyl-Mokhnatchuk@users.noreply.github.com>
2023-10-26 20:40:07 +0300

    Update and rename script.js to BubbleSort.js

---
+++ BubbleSort.js
@@ -0,0 +1,17 @@
+const { faker } = require('@faker-js/faker');
+
+let arr = Array.from(Array(20));
+
+arr = arr.map((i) => faker.person.firstName());
+
+for (let i = 0, tmp; i < arr.length - 1; i++) {
+    for (var j = 0; j < arr.length - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+            tmp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = tmp;
+        }
+    }
+}
+
+console.table(arr);
